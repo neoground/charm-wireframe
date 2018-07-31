095937bd5d617f7b6b4ae8b8fbf120c7e7706026
Sven Reifschneider <9088619+neoground@users.noreply.github.com>
2018-07-31 22:15:24 +0200

    App env updates

--- app/Engine.php
+++ app/Engine.php
@@ -24,7 +24,14 @@ class Engine extends EngineManager implements ModuleInterface
         // Set the environment for configuration
         //
         // This value must be identic with the sub directory
-        // inside app/Config/Environments
+        // inside app/Config/Environments.
+        //
+        // A file 'app.env' inside the app directory will
+        // override this value with it's content.
+        // Make sure this will be gitignored.
+        // This can be used e.g. on production servers where
+        // you deploy via deployer, git or something similar.
+
         $this->setEnvironment('Local');
     }
 
